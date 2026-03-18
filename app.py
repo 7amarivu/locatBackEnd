9bdf1fb3c710c8b6a3d81d7ba2484d7755be4ca5
Anand <97605821+7amarivu@users.noreply.github.com>
2026-03-18 17:32:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # allow all origins
 
-@app.route("/location", methods=["POST"])
+@app.route('/location', methods=['POST'])
 def location():
-    data = request.json
-    print("Received location:", data)  # You can save to DB here
+    data = request.get_json()
+    print("Received data:", data)
     return {"status": "ok"}
-
-if __name__ == "__main__":
-    app.run()
